9170cb231d56d35c58a05ccf61c591cf09925648
hehuizhu <eml_zhu@163.com>
2021-11-30 23:54:37 +0800

    learning stack

--- examples/poly.cpp
+++ examples/poly.cpp
@@ -43,31 +43,52 @@ void PrintPolyn(Polynomial P)
 
 void MergePolyn(Polynomial &PA, Polynomial &PB, Polynomial &PC)
 {
+    /**
+     * 当 PA 和 PB 都不是空链表时，判断 PA 和 PB 哪一个链表的头节点的指数值是否相等，
+     * 如果不相等，将较小值的节点添加到结果PC里，当一个节点被添加到结果里之后，将对应链表中的节点向后移一位。
+     * 如果相等，再判断结点的系数的值之和是否为0。如果是，删除这两个结点，否则，求和后删除其中一个。
+    */ 
     PC = new PNode;
     PC->next = nullptr;
 
-    PNode *pa, *pb, *pc;
+    PNode *pa, *pb, *pc, *p;
     pa = PA->next;
     pb = PB->next;
-    pc = PC->next;
+    delete PA;
+    delete PB;
+    PA = nullptr;
+    PB = nullptr;
+    pc = PC;
 
     while (pa && pb) {
         if (pa->expn < pb->expn) {
-            // insert pa
             pc->next = pa;
-            pa->next = pa
-        } else if (pa->expn = pb->expn) {
+            pa = pa->next;
+            pc = pc->next;
+        } else if (pa->expn == pb->expn) {
             if (pa->coef + pb->coef == 0) {
-                // free pa, free pb
+                p = pa;
+                pa = pa->next;
+                delete p;
+                p = pb;
+                pb = pb->next;
+                delete p;
             } else {
-                // insert pa+pb
+                pa->coef += pb->coef;
+                pc->next = pa;
+                pa = pa->next;
+                pc = pc->next;
+                p = pb;
+                pb = pb->next;
+                delete p;
             }
         } else {
-            // insert pb
+            pc->next = pb;
+            pb = pb->next;
+            pc = pc->next;
         }
-        pc = pc->next;
     }
-    // insert non-nullptr
+    pc->next = pa ? pa : pb;
 }
 
 int main()
@@ -78,10 +99,12 @@ int main()
     float B_coef[] = {8, 22, -9};
     int B_expn[] = {1, 7, 8};
 
-    Polynomial PA, PB;
+    Polynomial PA, PB, PC;
     CreatePolyn(PA, 4, A_coef, A_expn);
     CreatePolyn(PB, 3, B_coef, B_expn);
     PrintPolyn(PA);
     PrintPolyn(PB);
 
+    MergePolyn(PA, PB, PC);
+    PrintPolyn(PC);
 }
